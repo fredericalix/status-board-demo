c43fd2a4319c4229ad96cb58e2c16a7e37a1f712
Frédéric Alix <frederic.alix@pancasat.com>
2023-05-03 20:53:44 +0200

    Modification de la variable API_URL et Ajout de documentation

--- frontend-server/src/App.vue
+++ frontend-server/src/App.vue
@@ -1,76 +1,88 @@
 <template>
   <div id="app">
-    <div class="grid-container">
+    <!-- Création d'un conteneur pour les notifications -->
+    <div class="notification-container">
+      <!-- Boucle sur les notifications pour créer un carré pour chaque notification -->
       <div
-        v-for="status in statusList"
-        :key="status.id"
-        class="status-box"
-        :style="{ backgroundColor: status.state }"
+        v-for="(notification, index) in notifications"
+        :key="index"
+        class="notification"
+        :style="{ backgroundColor: notification.state }"
       >
-        <p>{{ status.designation }}</p>
+        <!-- Affichage de la désignation de la notification au centre du carré -->
+        <p>{{ notification.designation }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = "http://localhost:8080"; // Remplacez par l'URL de votre backend
+// Définition de l'URL de l'API backend en fonction de la variable d'environnement ou par défaut en utilisant "http://localhost:8080"
+const API_URL = process.env.API_URL || 'http://localhost:8080';
 
 export default {
+  name: 'App',
   data() {
     return {
-      statusList: [],
+      notifications: [],
     };
   },
   async created() {
+    // Récupération des notifications depuis l'API lors du chargement de l'application frontend
     try {
-      const response = await axios.get(`${API_URL}/status`);
-      this.statusList = response.data;
-      this.setupEventSource();
+      const response = await axios.get(`${API_URL}/notifications`);
+      this.notifications = response.data;
     } catch (error) {
-      console.error("Erreur lors de la récupération des données :", error);
+      console.error('Erreur lors de la récupération des notifications:', error);
     }
+
+    // Écoute des mises à jour des notifications via Server-Sent Events (SSE)
+    const eventSource = new EventSource(`${API_URL}/notifications/updates`);
+    eventSource.onmessage = (event) => {
+      const updatedNotification = JSON.parse(event.data);
+      this.updateNotification(updatedNotification);
+    };
   },
   methods: {
-    setupEventSource() {
-  const eventSource = new EventSource(`${API_URL}/events`);
-  eventSource.onmessage = () => { // Supprimez 'event' ici
-    this.fetchStatus();
-  };
-},
-    async fetchStatus() {
-      try {
-        const response = await axios.get(`${API_URL}/status`);
-        this.statusList = response.data;
-      } catch (error) {
-        console.error("Erreur lors de la récupération des données :", error);
-      }
+    // Mise à jour d'une notification dans la liste des notifications
+    updateNotification(updatedNotification) {
+      this.notifications = this.notifications.map((notification) => {
+        if (notification.id === updatedNotification.id) {
+          return updatedNotification;
+        }
+        return notification;
+      });
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
 body {
-  background-color: #1e1e1e;
-  margin: 0;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #2c3e50;
+  font-family: 'Source Sans Pro', sans-serif;
 }
 
-.grid-container {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  gap: 20px;
-  padding: 20px;
+.notification-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  padding: 2rem;
 }
 
-.status-box {
-  background-color: #eee;
-  border-radius: 10px;
-  padding: 20px;
+.notification {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  border-radius: 8px;
+  font-size: 1.2rem;
+  color: #fff;
   text-align: center;
-  color: white;
 }
 </style>
