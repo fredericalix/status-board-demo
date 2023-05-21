e22f2b1c7d575bc98f1b60f1ad3272687e41e612
Frédéric Alix <frederic.alix@pancasat.com>
2023-05-21 12:47:08 +0200

    add API_URL in vue config

--- frontend-server/vue.config.js
+++ frontend-server/vue.config.js
@@ -1,4 +1,15 @@
+const webpack = require('webpack');
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    API_URL: JSON.stringify(process.env.API_URL)
+                }
+            })
+        ]
+    }
 })
